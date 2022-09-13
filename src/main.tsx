c0cfc5310605e1ceac42a0352eb08b210488adc6
jensmtg <jm.gleditsch@gmail.com>
2022-09-13 23:21:44 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { Plugin } from 'obsidian';
 import { ObsidianInfluxSettingsTab } from './settings';
-import { asyncDecoBuilderExt  } from './cm6/asyncViewPlugin';
+import { asyncDecoBuilderExt } from './cm6/asyncViewPlugin';
 
 
 export interface ObsidianInfluxSettings {
@@ -20,9 +20,9 @@ const DEFAULT_SETTINGS: Partial<ObsidianInfluxSettings> = {
 	sortingPrinciple: 'NEWEST_FIRST',
 	sortingAttribute: 'ctime',
 	showBehaviour: 'OPT_OUT',
-	exclusionPattern:[],
-	inclusionPattern:[],
-	collapsedPattern:[],
+	exclusionPattern: [],
+	inclusionPattern: [],
+	collapsedPattern: [],
 	listLimit: 0,
 	variant: 'CENTER_ALIGNED',
 	fontSize: 13,
@@ -35,12 +35,16 @@ export default class ObsidianInflux extends Plugin {
 	settings: ObsidianInfluxSettings;
 
 	async onload(): Promise<void> {
-		
-		this.registerEditorExtension(asyncDecoBuilderExt)
 
 		await this.loadSettings();
 
+		this.registerEditorExtension(asyncDecoBuilderExt)
+
 		this.addSettingTab(new ObsidianInfluxSettingsTab(this.app, this));
+
+		this.registerEvent(this.app.vault.on('modify', (file) => { console.log('modified', file); this.redraw() }));
+		this.registerEvent(this.app.vault.on('rename', (file) => { console.log('rename', file); this.redraw() }));
+		this.registerEvent(this.app.vault.on('delete', (file) => { console.log('delete', file); this.redraw() }));
 
 	}
 
@@ -54,6 +58,57 @@ export default class ObsidianInflux extends Plugin {
 	}
 
 	async onunload() {
+	}
+
+	redraw() {
+		console.log('redraw')
+		// const views = this.app.workspace.iterateAllLeaves()
+		this.app.workspace.iterateRootLeaves(leaf => {
+
+			const leafType: 'preview' | 'source' = leaf.view?.currentMode?.type
+
+			if (leafType === 'preview') {
+
+				console.log('leaf', leaf.view?.currentMode?.type)
+				console.log('leaf2', leaf.view?.file)
+				console.log('leaf3', leaf)
+
+				const div = document.createElement("div")
+				const textnode = document.createTextNode("Influx goes here!");
+				div.setAttr("id", "influx-component")
+				div.appendChild(textnode)
+				const container: HTMLDivElement = leaf.containerEl
+				const sel = container.querySelector(".markdown-preview-section");
+				const host = sel.childNodes[sel.childNodes.length - 1]
+				console.log('sel', sel)
+				console.log('host', host)
+
+				host.appendChild(div)
+
+			}
+
+
+			/** 
+			 * TODO.
+			 * This spike implementation proves the possibility of access to update
+			 * preview leaves upon relevant triggers (opening, editing).
+			 * Next steps:
+			 * - Access to refresh editor leaves in the same vein as well.
+			 * - More complete logic:
+			 *   - considerRerenders triggered by different signals (editing, rename)
+			 *   - evaulate each root leaf
+			 *   - if preview:
+			 *     - if force rerender (settings changes)
+			 *     - if file is, or has backlink to, signal origin
+			 *     - produce and render a influx component and place it instead or anew.
+			 *   - if source:
+			 *     - if force rerender (settings changes)
+			 *     - if has backlink to signal origin
+			 *     - force refresh of leaf (or influx component only?)
+			 */
+
+		})
+		// console.log('views', views)
 	}
 
 }
