b6eae8a5e88e921905756c9482bdf317c5e3745d
semanticdata <contact@miguelpimentel.do>
2026-03-07 17:29:51 -0600

    remove redundant control

--- src/ui/influx-react-component.tsx
+++ src/ui/influx-react-component.tsx
@@ -362,21 +362,6 @@ export default function InfluxReactComponent(props: InfluxReactComponentProps): 
 									)}
 								</div>
 							)}
-							<button
-								type="button"
-								className="influx-icon-button influx-toolbar-button"
-								aria-label={allVisibleComponentsCollapsed ? 'Expand all' : 'Collapse all'}
-								onClick={() => toggleAll()}
-							>
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="influx-svg-icon influx-svg-icon--expand-collapse-all">
-									<polyline points="8 18 12 22 16 18">
-									</polyline>
-									<polyline points="8 6 12 2 16 6">
-									</polyline>
-									<line x1="12" y1="2" x2="12" y2="22">
-									</line>
-								</svg>
-							</button>
 							<details className="influx-toolbar-menu">
 								<summary className="influx-icon-button influx-toolbar-button" aria-label="More Influx actions">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="influx-svg-icon influx-svg-icon--more">
